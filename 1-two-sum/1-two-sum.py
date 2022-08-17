c1e214d09d41c394a919db04eb06b7e2cc0181ee
Tamir Rozenfeld <108065131+TamirRozenfeld@users.noreply.github.com>
2022-08-17 09:10:15 +0300

    Time: 76 ms (66.90%), Space: 14.4 MB (45.09%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,20 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[j] == target - nums[i]:
-                    return [i, j]
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+        
+        hashmap = {}
+        
+        for i, n in enumerate(nums):
+            diff = target - n 
+            if diff in hashmap:
+                return [hashmap[diff],i]
+            hashmap[n] = i
+        return
+        
+            
+            
+            
+            
+        
+        
+       
         
