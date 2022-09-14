502cdbc8f6d0c4ff87b41e2cfbd112e2214442a2
Tamir Rozenfeld <108065131+TamirRozenfeld@users.noreply.github.com>
2022-09-14 13:24:42 +0300

    Time: 549 ms (61.12%), Space: 14.3 MB (98.48%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        l, r = 1, max(piles)
+        k = 0
+
+        while l <= r:
+            m = (l + r) // 2
+
+            totalTime = 0
+            for p in piles:
+                totalTime += ((p - 1) // m) + 1
+            if totalTime <= h:
+                k = m
+                r = m - 1
+            else:
+                l = m + 1
+        return k
+        
+        
